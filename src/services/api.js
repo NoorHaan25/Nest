7350dea172c074da62e525321b8bebd7a318016a
NoorHaan25 <nourhantaha588@gmail.com>
2023-09-25 03:56:20 +0300

    Modified BASE_URL (API)

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://api-xh9w.onrender.com";
 export const getAllPosts = () => {
   return axios.get(`${BASE_URL}/postsAll`);
 };
